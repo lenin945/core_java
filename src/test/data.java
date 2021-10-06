a4b30d82f9fd646d74219c53a68e2acdb65c2507
lenin945 <leninmathiyarasi@gmail.com>
2021-10-06 15:08:20 +0530

    core java

---
+++ src/test/data.java
@@ -0,0 +1,12 @@
+package test;
+
+public class data {
+public static void main(String[] args) {
+	
+	int []a= {1,2,3,4,5,6,3,5,34};
+	
+	for (int i = 0; i < a.length; i++) {
+		System.out.println(a);
+	}
+}
+}
