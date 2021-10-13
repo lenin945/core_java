f4951c9c3b23b3dfbcc906d0f5c7b9632dab315b
lenin945 <leninmathiyarasi@gmail.com>
2021-10-13 11:36:57 +0530

    Updated

--- src/test/data.java
+++ src/test/data.java
@@ -3,10 +3,22 @@ package test;
 public class data {
 public static void main(String[] args) {
 	
-	int []a= {1,2,3,4,5,6,3,5,34};
+	StringBuffer s = new StringBuffer("Lenin");
 	
-	for (int i = 0; i < a.length; i++) {
-		System.out.println(a);
+	StringBuffer s1= s.append(8);
+	
+	System.out.println(s1);
+	
+//	int l = s.capacity();
+//	System.out.println(l);
+//	
+//	StringBuilder s2 = new StringBuilder("Satz");
+	
+	int a = 0, b = 1; System.out.println(a); System.out.println(b);
+	for (int i = 2; i <= 10; i++) { int c = a + b; System.out.println(c);
+	a = b; b = c;
 	}
+	
+		
 }
 }
